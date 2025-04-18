fee2282016ec218b1ac06159f1d5ec0def7a26f2
Sahil Kasare <cdsahilkasare@gmail.com>
2025-04-19 01:18:30 +0530

    Time: 28 ms (57.31%), Space: 55.5 MB (21.03%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int mod = (int)1e9 + 7;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] pow = new int[n];
+        pow[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow[i] = (pow[i - 1] * 2) % mod;
+        }
+
+        int i = 0, j = n-1;
+        int count = 0;
+        while (i <= j) {
+            if (nums[i] + nums[j] <= target) {
+                count = (count + pow[j - i]) % mod;
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return count;
+    }
+}
