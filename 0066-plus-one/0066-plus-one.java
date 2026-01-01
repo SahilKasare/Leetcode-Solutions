0efb57e26faf214c8f37165ccc636022661130e9
Sahil Kasare <cdsahilkasare@gmail.com>
2026-01-02 02:56:25 +0530

    Time: 0 ms (100%), Space: 43.6 MB (7.68%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+
+        while (i >= 0) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+            i--;
+        }
+
+        // If we reach here, all digits were 9
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
